279c17902376f9ed3b2f4c320ef0cfb46675cd16
Myron Turner <turnermm02@shaw.ca>
2013-07-01 19:03:59 -0500

    Added label for settings

---
+++ lang/en/settings.php
@@ -0,0 +1,2 @@
+<?php
+$lang['platform'] = 'Select on which platforms you want to implement section toggling: none,mobile, all. With <b>all</b>, you will get section toggling on standard desktop systems.';
