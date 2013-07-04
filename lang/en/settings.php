712ace8aa9bb3567a55a560bff88b313c751596a
Myron Turner <turnermm02@shaw.ca>
2013-07-03 19:26:48 -0500

    Updated settings.php

--- lang/en/settings.php
+++ lang/en/settings.php
@@ -3,4 +3,4 @@ $lang['platform'] = 'This selects the platforms on which you want to implement s
 $lang['type'] ="To use a template other than the standard dokuwiki template, you must indicate whether the div which encloses the affected headers is to be identified by either a class or an id. Then below, you must enter the name of the class or id.";
 $lang['name'] ="Name of the class or id which identifies the enclosing div.";
 $lang['headers'] ="Smallest header to activate as toggle.   All headers between <b>h1</b> and this setting will be toggles. The default is <b>h4</b>.";
-
+$lang['suspend'] ="If set to true the plugin will be inactivated and no toggles will be set.";
