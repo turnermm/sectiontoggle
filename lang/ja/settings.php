56a19333998c6238dca6e35541a8b64b54a0a495
Hideaki SAWADA <chuno@live.jp>
2015-12-17 22:50:05 +0100

    translation update

---
+++ lang/ja/settings.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Hideaki SAWADA <chuno@live.jp>
+ */
+$lang['platform']              = 'Sectiontoggle を実装したいプラットフォームを選択する： <b>none</b>・<b>mobile</b>・<b>all</b>。<b>all</b> の場合、標準的なデスクトップシステムでの Sectiontoggle になります。<b>mobile</b> には画面の小さな携帯電話とタブレットを含みます。';
+$lang['type']                  = 'DokuWiki の標準以外のテンプレートを使用する場合、影響のあるヘッダーを囲む div を識別する class か id を指定する必要があります。次に class か id の名前を入力して下さい。';
+$lang['name']                  = '囲む div を識別する class か id の名前';
+$lang['headers']               = 'Sectiontoggle を有効にする最小ヘッダー。<b>h1</b> からこの設定までの全ヘッダーを切替スイッチ化します。デフォルト値は <b>h4</b>。';
+$lang['suspend']               = '真の場合、プラグインが無効化され、切替スイッチが設定されません。';
+$lang['xcl_headers']           = '切替スイッチ化すべきでないヘッダーを指定する。';
