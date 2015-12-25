963a797c1de5711992d7d341a4486acdd7e0e75e
Myron Turner <turnermm02@shaw.ca>
2015-12-25 09:36:11 -0600

    brought <action>::register declaration into conformity with PHP 7.0

--- action.php
+++ action.php
@@ -11,7 +11,7 @@ class action_plugin_sectiontoggle extends DokuWiki_Action_Plugin {
     /**
      * Register its handlers with the DokuWiki's event controller
      */
-    function register(&$controller) {
+    function register(Doku_Event_Handler $controller) {
         $controller->register_hook('DOKUWIKI_STARTED', 'AFTER', $this, '_jsinfo');
     }
 
