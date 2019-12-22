a3421b41de53bf78c2b0bcbf8f7b2afdb291af5d
Myron Turner <turnermm02@shaw.ca>
2019-12-22 08:22:17 -0600

    adds settings incl_ns lang string

--- lang/en/settings.php
+++ lang/en/settings.php
@@ -10,4 +10,5 @@ $lang['tablet_alt'] = 'Use the alternate template for tablets';
 $lang['tablet_toggle'] = 'Use toggles on tablets as well as phones';
 $lang['xcl_ns'] = 'Comma separated list of namespaces to be excluded from toggles, without initial or final colons(i.e. <code>&nbsp;name:space&nbsp;</code>,  not<code>&nbsp;:name:space:&nbsp;</code>) ';
 $lang['xcl_pg'] = 'Comma separated list of  page ids to be excluded from toggles (without initial colons (i.e <code>&nbsp;page:id &nbsp;</code>,  not<code>&nbsp;:page:id&nbsp;</code> )';
-$lang['incl_id'] = 'Comma separated list of page and namespace ids where toggles should appear, without initial or final colons. If this option is set, the <code>xcl_ns</code> and <code>xcl_pg</code> options will be ignored.'; 
+$lang['incl_id'] = 'Comma separated list of page ids where toggles should appear, without initial or final colons. If this option is set, the<code>xcl_pg</code> option will be ignored.'; 
+$lang['incl_ns'] = 'Comma separated list of page ids where toggles should appear, without initial or final colons. If this option is set, the<code>xcl_ns</code> option will be ignored.'; 
