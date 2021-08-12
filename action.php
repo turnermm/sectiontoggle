b0bcdc819c730d3ccb95725df95ea5af1f4d37a1
Myron Turner <turnermm02@shaw.ca>
2021-08-12 18:10:29 -0500

    synched master with old-master; reverts to pre-toc fixes

--- action.php
+++ action.php
@@ -36,7 +36,6 @@ class action_plugin_sectiontoggle extends DokuWiki_Action_Plugin {
              $JSINFO['se_suspend']=1;                           
 		     return;
         }
-       $JSINFO['toc_suspend'] = strpos($id_inc, $ID);
        
         $NS = implode("|",$this->normalize_names($this->getConf('xcl_ns'),true));
         $JSINFO['se_suspend']=0;
